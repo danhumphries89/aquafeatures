440550a7c1f3c1e61098573a062bbc2b944e52be
Dan Humphries <dan.humphries89@gmail.com>
2013-01-02 19:28:59 +0000

    basics setup and fonts installed

---
+++ wp-content/themes/Broadway/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+/** Add Menus that we wish to use **/
+function registerMenus() {
+  register_nav_menus(
+  	array( 
+  		'mainmenu' => __( 'Main Menu' ),
+    )
+  );
+}
+add_action( 'init', 'registerMenus' );
+
+?>
--- wp-content/themes/Broadway/header.php
+++ wp-content/themes/Broadway/header.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html>
-<html charset="utf-8">
+<html <?php language_attributes(); ?>>
 <head>
-<title></title>
-<link href="/assets/css/base.css" rel="stylesheet" type="text/css" />
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<title> <?php wp_title('|', true, 'right'); bloginfo( 'name' ); ?> </title>
+
+<link href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/fonts.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/base.css" rel="stylesheet" type="text/css" />
+
 </head>
 <body>
+	<h1>Testing</h1>
