b8fee89c95f5e409fcd4518b9cd4f8d35d9a7f74
Dan Humphries <dan.humphries89@gmail.com>
2013-01-12 13:39:44 +0000

    Updates to responsive design to counter zooming effect

--- wp-content/themes/Broadway/header.php
+++ wp-content/themes/Broadway/header.php
@@ -9,7 +9,7 @@
 <link href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/base.css" rel="stylesheet" type="text/css" screen="all" />
 
 <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/jquery-1.8.3.min.js"></script>
-<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/script.js"></script>
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/script.js" async="async"></script>
 
 <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/flexslider/jquery.flexslider-min.js"></script>
 <link href="<?php echo get_stylesheet_directory_uri(); ?>/assets/flexslider/flexslider.css" rel="stylesheet" type="text/css" />
